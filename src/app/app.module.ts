2c5049be26f47e2ec20a0da7993b7aea3bc14d42
Muammer Yilmaz <muammercan68@gmail.com>
2022-09-28 16:00:33 +0300

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CategoryComponent } from './components/category/category.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ProductComponent } from './components/product/product.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CategoryComponent,
+    NavComponent,
+    ProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
